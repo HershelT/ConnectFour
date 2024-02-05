d2d9a238c9a256936d2ff6989876b4dcfa879f33
= <hershelthomas@gmail.com>
2024-02-05 19:19:13 +0200

    Connect 4 Game - Uses Hershel Thomas's 2d anscii graphics engine for terminal

---
+++ ImageReader/ImageProcessor.py
@@ -0,0 +1,107 @@
+# Import the Pillow library
+# from PythonTerminalSprites import *
+from PIL import Image, ImageChops
+import os
+import re
+from AnsiiEscapeColors import *
+# from numpy import  array, reshape, asarray, ndarray
+import numpy as np
+
+def generate_ansi_colors():
+    basic_colors = [(0, 0, 0), (128, 0, 0), (0, 128, 0), (128, 128, 0),
+                    (0, 0, 128), (128, 0, 128), (0, 128, 128), (192, 192, 192),
+                    (128, 128, 128), (255, 0, 0), (0, 255, 0), (255, 255, 0),
+                    (0, 0, 255), (255, 0, 255), (0, 255, 255), (255, 255, 255)]    
+    levels = [0, 95, 135, 175, 215, 255]
+    extended_colors = [(r, g, b) for r in levels for g in levels for b in levels]    
+    return basic_colors + extended_colors
+ansi_colors = generate_ansi_colors()
+
+class pixelImage:
+    @staticmethod
+    def trim_image(img):
+    # Convert the image to RGB if it's not already
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+
+        # Create a background image of the same color as the corner pixel
+        bg = Image.new('RGB', img.size, img.getpixel((0,0)))
+
+        # Find the difference between the input image and the background image
+        diff = ImageChops.difference(img, bg)
+
+        # The difference is zero for all pixels that match the background color,
+        # so we need to find the bounding box of the non-zero regions
+        bbox = diff.getbbox()
+
+        # If the bounding box is not None, crop the image to that bounding box
+        if bbox:
+            img = img.crop(bbox)
+
+        return img
+    def __init__(self, img : list , scaleRatio = False):
+        imageList = []
+        self.ImageAnscii = []
+        for image in img:
+            imageList.append(self.trim_image(Image.open(image)))
+            # imageList.append(Image.open(image))
+        for rgb in imageList:
+            self.ImageAnscii.append(self.getPixelToAnscii(rgb, scaleRatio))
+
+    def getAnsciiList(self):
+        return self.ImageAnscii
+    def size(self, image):
+        width, height = image.size
+        return width, height
+    def rgb_to_anscii(self, r, g, b):
+        def distance(c1, c2):
+            return sum((x1-x2)**2 for x1,x2 in zip(c1, c2))
+        rgb_color = (r, g, b)
+        # Find the index of the closest color in the ansi_colors list
+        closest_color = min(range(len(ansi_colors)), key=lambda index: distance(rgb_color, ansi_colors[index]))
+        # Return the ANSI color code
+        return "\033[48;5;{}m".format(closest_color)
+    def getPixelToAnscii(self, image, scaleRatio = False):
+        # Convert the image to RGB if it's not already
+        if image.mode != 'RGB':
+            image = image.convert('RGB')
+        sizes = self.size(image)
+        ansi_codes = []
+        for x in range(sizes[1]):
+            row = []
+            for y in range(sizes[0]):
+                r, g, b = image.getpixel((y, x))
+                row.append(self.rgb_to_anscii(*(r,g,b)) + ' ')
+                if scaleRatio:
+                    row.append(self.rgb_to_anscii(*(r,g,b)) + ' ')
+            ansi_codes.append(row)
+        # ansi_codes = list(map(lambda pixel: (self.rgb_to_anscii(*pixel) + ' ') *(2 if scaleRatio else 1), pixels))
+        # Convert the list of ANSI codes to a 2D numpy array and return it
+        return ansi_codes
+        # return [ansi_codes[i*width:(i+1)*width] for i in range(height)]
+        # return np.array(ansi_codes).reshape(img.size[1], img.size[0])
+    def printOutImage(self, imageAnscii : list):
+        # drawing = self.colors.tolist()  
+        print('\033[0m\n'.join(''.join(row) for row in imageAnscii), end=reset)
+    def printOutNumpy(self, imageAnscii : list):
+        print(np.array(imageAnscii))
+    def getPixelArray(self, imageAtSpot):
+        return self.ImageAnscii[imageAtSpot]
+
+# Convert the 2D array to a string
+
+
+
+# Check if the operating system is Windows
+is_windows = os.name == 'nt'
+dir_sep = '\\' if is_windows else '/'
+
+#declare all the images
+ConnectFourBoard = [f'ImageReader{dir_sep}PythonTerminalSprites{dir_sep}ConnectFourBoard1.png', f'ImageReader{dir_sep}PythonTerminalSprites{dir_sep}ConnectFourBoard2.png' ]
+ConnectFourPieces = [f'ImageReader{dir_sep}PythonTerminalSprites{dir_sep}ConnectFourPieces1.png', f'ImageReader{dir_sep}PythonTerminalSprites{dir_sep}ConnectFourPieces2.png']
+
+#create a pixel image object
+ConnectFourBoard = pixelImage(ConnectFourBoard, True)
+ConnectFourPieces = pixelImage(ConnectFourPieces, True)
+
+
---
+++ main.py
@@ -0,0 +1,190 @@
+import os, sys
+sys.path.insert(0, 'ImageReader')
+from ImageReader import *
+from loadscreen import *
+from functions import *
+LENGTHOFBOARD = 6
+WIDTHOFBOARD = 7
+#USing lambda to clear the screen 
+clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+#Creating the piece class
+class Piece:
+    def __init__(self, color):
+        self.color = color
+    def __str__(self):
+        return self.color
+    def __repr__(self):
+        return self.color
+#Creating the board class
+class Board:
+    def __init__(self):
+        self.board = [[0 for i in range(WIDTHOFBOARD)] for j in range(LENGTHOFBOARD)]
+    def placePiece(self, piece, BoardPieceColor, x):
+        for i in range(0, len(self.board)):
+            if isinstance(self.board[0][x], Piece):
+                return False
+            elif isinstance(self.board[i][x], Piece) and i != 0:
+                self.board[i-1][x] = piece
+                addLinesToSreen(BoardPieceColor, BoardConnect, 6*(len(self.board) - (i-1)-1)+1, x*12 + 2, '\033[m', False)
+                addLinesToSreen(BoardConnect, EmptyScreen, 6, 10, '\033[m', False)
+                return True
+            elif i == len(self.board)-1:
+                self.board[i][x] = piece
+                addLinesToSreen(BoardPieceColor, BoardConnect, 12*(len(self.board) - (i)-1)+1, x*12 + 2, '\033[m', False)
+                addLinesToSreen(BoardConnect, EmptyScreen, 6, 10, '\033[m', False)
+                return True
+    def checkWin(self):
+        for i in range(0, len(self.board)):
+            for j in range(0, len(self.board[i])):
+                if isinstance(self.board[i][j], Piece):
+                    if self.checkWinPiece(i, j):
+                        return True
+        return False
+    def checkWinPiece(self, x, y):
+        piece = self.board[x][y]
+        if self.checkWinVertical(x, piece):
+            return True
+        if self.checkWinHorizontal(x, y, piece):
+            return True
+        if self.checkWinDiagonal(x, y, piece):
+            return True
+        return False
+    def checkWinVertical(self, x, piece):
+        count = 0
+        for i in range(0, len(self.board)):
+            if isinstance(self.board[i][x], Piece) and self.board[i][x].color == piece.color:
+                count += 1
+                if count == 4:
+                    return True
+            else:
+                count = 0
+        return False
+    def checkWinHorizontal(self, x, y, piece):
+        count = 0
+        for j in range(0, len(self.board[y-1])):
+            if isinstance(self.board[x][j], Piece) and self.board[x][j].color == piece.color:
+                count += 1
+                if count == 4:
+                    return True
+            else:
+                count = 0
+        return False
+    def checkWinDiagonal(self, x, y, piece):
+        for i in range(0, len(CHECKDIAGONALS)):
+            count = 0
+            for j in range(0, len(self.board)):
+                if x+CHECKDIAGONALS[i][0]*j < 0 or x+CHECKDIAGONALS[i][0]*j >= len(self.board) or y+CHECKDIAGONALS[i][1]*j < 0 or y+CHECKDIAGONALS[i][1]*j >= len(self.board[0]):
+                    break
+                if isinstance(self.board[x+CHECKDIAGONALS[i][0]*j][y+CHECKDIAGONALS[i][1]*j], Piece) and self.board[x+CHECKDIAGONALS[i][0]*j][y+CHECKDIAGONALS[i][1]*j].color == piece.color:
+                    count += 1
+                    if count == 4:
+                        return True
+                else:
+                    count = 0
+    def __str__(self):
+        result = ""
+        for row in self.board:
+            for col in row:
+                if col == 0:
+                    result += f"{black}|{reset}" + "0" + f"{black}|{reset}"
+                else:
+                    result += f"{black}|{reset}" + col.color + f"{black}|{reset}"
+            result += "\n"
+        result += f"{background_bright_blue}_____________________{reset}\n 0  1  2  3  4  5  6"
+        return result
+    def __repr__(self):
+        return self.__str__()
+
+#Function to move the piece to the board and check if the game is won
+def MovePiece(col, type):
+    if type[0] == "red":
+        BoardPieceColor = PIECERED
+    else:
+        BoardPieceColor = PIECEYELLOW
+    if not b.placePiece(Piece(f'{type[1]} \033[0m'), BoardPieceColor ,col):
+        return False
+    elif b.checkWin():
+        clear()
+        printScreen(EmptyScreen)
+        print(reset)
+        print(f"{type[0]} wins")
+        sys.exit()
+
+#System for using arrow keys to drop pieces in the board in graphics
+def selectingCol(piece):
+    col = 3
+    currentCol = 12 * (col + 1)
+    key_listener = MyKeyListener()
+    listener = keyboard.Listener(
+            on_press=key_listener.on_press,
+            on_release=key_listener.on_release)
+    listener.start()
+    emptyString = convert_2d_array_to_empty_strings(piece)
+    addLinesToSreen(piece, EmptyScreen, ROWOFDISPLAY, currentCol, '\033[m', False)
+    printScreen(EmptyScreen)
+    while not key_listener.is_enter_pressed() and not key_listener.is_down_arrow_pressed():
+        if (key_listener.is_left_arrow_pressed() and col > 0) or (key_listener.is_right_arrow_pressed() and col < 6):
+            if key_listener.is_right_arrow_pressed():
+                num = [12,1]
+            else:
+                num = [-12,-1]
+            addLinesToSreen(emptyString, EmptyScreen, ROWOFDISPLAY, currentCol, '\033[m', False)
+            currentCol += num[0]
+            col += num[1]
+            addLinesToSreen(piece, EmptyScreen, ROWOFDISPLAY, currentCol, '\033[m', False)
+            key_listener.keys_pressed.discard(all); 
+            printScreen(EmptyScreen)
+            time.sleep(0.15)
+        if key_listener.is_esc_pressed():
+            print("You have exited the program")
+            sys.exit()
+        if key_listener.is_c_pressed():
+            clear()
+            printScreen(EmptyScreen)
+    addLinesToSreen(convert_2d_array_to_empty_strings(PIECERED), EmptyScreen, ROWOFDISPLAY, currentCol, '\033[m', False)
+    listener.stop()
+    del listener
+    return col   
+ 
+#Creating Final variables for the display of the two pieces and how the 4 in a row gets checked
+CHECKDIAGONALS = [(1, 1), (-1,1), (1, -1), (-1, -1)]
+PIECEYELLOW = ConnectFourPieces.getPixelArray(0)
+PIECERED = ConnectFourPieces.getPixelArray(1)
+
+#Initializing the board graphics from the pixel art images
+BoardConnect = ConnectFourBoard.getPixelArray(0)
+EmptyScreen = ConnectFourBoard.getPixelArray(1)
+ROWOFDISPLAY = len(EmptyScreen)-len(PIECEYELLOW)
+
+
+#Initializing the board and pieces for behind the scenes game logic as well as the turn counter
+turn = 0
+b = Board()
+p = Piece(f'{background_bright_red} \033[0m')
+
+# Displaying the board
+addLinesToSreen(BoardConnect, EmptyScreen, 6, 10, '\033[m', False)
+clear()
+printScreen(EmptyScreen)
+print(reset)
+
+# Starting Game Loop to play the game
+if __name__ == "__main__":
+    while True:
+        if turn % 2 == 0:
+            pieceColor = ["red", background_bright_red, PIECERED]
+        else:
+            pieceColor = ["yellow", background_yellow, PIECEYELLOW]
+        # clear()
+        printScreen(EmptyScreen)
+        # printScreen(BoardConnect)
+        colForPiece = selectingCol(pieceColor[2])
+        if  MovePiece(colForPiece, pieceColor) == False:
+            # turn -= 1
+            continue
+        turn += 1
+
+
+
+
+    
